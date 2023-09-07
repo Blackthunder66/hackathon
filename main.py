0862e37cd9c0c0e61f847752f03b57ff6ecfc2b7
Tanguy Lourme <tanguy.lourme@gmail.com>
2023-09-07 10:11:47 +0200

    bouton_1

--- main.py
+++ main.py
@@ -2,27 +2,40 @@ from flask import Flask
 
 from flask import render_template, request
 
-from src.utils.ask_question_to_pdf import split_text, ask_question_to_pdf, read_pdf, document, chunks, text
+from src.utils.ask_question_to_pdf import (
+    split_text,
+    ask_question_to_pdf,
+    read_pdf,
+    document,
+    chunks,
+    text,
+)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello(name=None):
-    return render_template('index.html', name=name)
+    return render_template("index.html", name=name)
 
-@app.route("/question", methods = ["GET"])
-def ask_question() :
-    question = ask_question_to_pdf(question = "pose moi une question sur le texte", textpdf = document)
-    return {"answer" : question}
 
-@app.route("/prompt", methods = ["POST"])
-def prompt() :
+@app.route("/question", methods=["GET"])
+def ask_question():
+    question = ask_question_to_pdf(
+        question="pose moi une question sur le texte", textpdf=document
+    )
+    return {"answer": question}
+
+
+@app.route("/prompt", methods=["POST"])
+def prompt():
     question = request.form["prompt"]
     response = ask_question_to_pdf(question)
     return {"answer": response}
 
-@app.route("/answer", methods = ["POST"])
-def answer() :
+
+@app.route("/answer", methods=["POST"])
+def answer():
     answer = request.form["prompt"]
-    response = ask_question_to_pdf(answer + "est-ce vrai ou faux?", textpdf = document)
+    response = ask_question_to_pdf(answer + "est-ce vrai ou faux?", textpdf=document)
     return {"answer": response}
